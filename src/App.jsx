a5b47104074fa8690b9f2c5f975d34f1d0d44007
Rufai Sanni <sruffy07@gmail.com>
2024-01-23 14:08:03 +0100

    initial ommit to the shopping-cart project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+
+import SHoppingCart from "./components/ShoppingCart";
+
+// import viteLogo from '/vite.svg'
+function App() {
+  return (
+    <>
+      <SHoppingCart />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ShoppingCart.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+
+export default function SHoppingCart() {
+  const [cartItems, setCartitems] = useState([]);
+  return (
+    <>
+      <h1>Cart List</h1>
+    </>
+  );
+}
